3718f8b57bd3598feab35f0f94f96a7cbd1d4bef
Mickael <mickael.spilmont.fr@gmail.com>
2019-10-02 15:56:02 +0200

    Ajout d'une requette de recherche de carte par nom

--- Api/ScryfallApi.js
+++ Api/ScryfallApi.js
@@ -3,3 +3,9 @@ export function getCardsByDate(page) {
         .then(response => response.json())
         .catch(error => console.error(error));
 }
+
+export function searchCardByNAme(cardName) {
+    return fetch(`https://api.scryfall.com/cards/named?fuzzy=${cardName}`)
+        .then(response => response.json())
+        .catch(error => console.error(error));
+}
